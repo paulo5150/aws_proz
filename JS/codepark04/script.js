db6b150b157a44e6b3c1c5c1129ade6c34b82b28
Paulo Lima <108300157+paulo5150@users.noreply.github.com>
2023-10-02 21:17:32 -0300

    renomeado o arquivo 'codepark01.js' para 'codepark03.js e criação da pasta e arquivos do codepark04'

---
+++ JS/codepark04/script.js
@@ -0,0 +1,6 @@
+function conexaoOk () {
+    for (let index = 0; index < 3; index++) {
+        console.log("Conexão feita com Sucesso!")
+    }
+}
+conexaoOk();
---
+++ JS/primeiro-js/script.js
@@ -0,0 +1,4 @@
+console.log("Hello world");
+let num3 = 15;
+let num4 = 27;
+console.log(num3+num4);

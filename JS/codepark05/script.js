e2b385b12de1b2d760e285d68e28c0f8268ea64e
¨paulo5150¨ <paulo5150@live.com>
2023-10-05 15:58:31 -0300

    criação da pasta e arquivos para o codepark05

---
+++ JS/codepark05/script.js
@@ -0,0 +1,7 @@
+const h1Titulo = document.querySelector('#titulo');
+h1Titulo.innerText = "Modelo H1";
+console.log(h1Titulo.innerText);
+
+const aLink = document.querySelector("a")
+aLink.innerText = "clique aqui";
+console.log(aLink.innerText);

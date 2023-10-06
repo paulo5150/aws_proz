19a8a45f58e040f4336febc89fcee7f2eb33123b
Paulo Lima <paulo5150@live.com>
2023-10-05 21:38:55 -0300

    conclusão da atividade codepark05 e atualização do repositório aws_proz

--- JS/codepark05/script.js
+++ JS/codepark05/script.js
@@ -1,7 +1,20 @@
-const h1Titulo = document.querySelector('#titulo');
-h1Titulo.innerText = "Modelo H1";
-console.log(h1Titulo.innerText);
+let h1Titulo = document.querySelector('#titulo');
+let aLink = document.querySelector("a")
+let listaNaoOrdenada = document.querySelector("ul")
+let listaOrdenada = document.querySelector("#lista-ordenada")
 
-const aLink = document.querySelector("a")
-aLink.innerText = "clique aqui";
+h1Titulo.innerText = "Modelo H1";
+aLink.innerText = "Proz Educação";
+listaNaoOrdenada.innerHTML = `
+<li>Item 01</li>
+<li>Item 02</li>
+<li>Item 03</li>`;
+listaOrdenada.innerHTML = ` 
+<li><a href="https://prozeducacao.com.br/">Proz</a></li>
+<li><a href="https://www.uol.com.br/">Uol</a></li>
+<li><a href="https://aws.amazon.com/pt/">AWS</a></li>`;
+
+console.log(h1Titulo.innerText);
 console.log(aLink.innerText);
+console.log(listaNaoOrdenada.innerHTML);
+console.log(listaOrdenada.innerHTML);
